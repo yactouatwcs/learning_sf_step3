630c11dce340763e6d239e326157a3884dbf7925
yactouat <yactouat@hotmail.com>
2022-11-21 22:19:56 +0100

    autowiring offer repository in price calculator

--- src/Service/PriceCalculator.php
+++ src/Service/PriceCalculator.php
@@ -2,16 +2,21 @@
 
 namespace App\Service;
 
-use App\Entity\Offer;
 use App\Entity\Product;
 use App\Entity\User;
+use App\Repository\OfferRepository;
 
 class PriceCalculator
 {
-   public function personalPrice(User $user, Product $product, Offer $offer): float
-   {
-       $currentUserDiscount = $user->getLoyaltyDiscount();
-       $todaysDiscount = floatval($offer->getDiscount()) ?? 1;
-       return $product->getPrice() * min($currentUserDiscount, $todaysDiscount);
-   }
+
+    public function __construct(private OfferRepository $offerRepository)
+    {
+    }
+
+    public function personalPrice(User $user, Product $product): float
+    {
+        $currentUserDiscount = $user->getLoyaltyDiscount();
+        $todaysDiscount = floatval($this->offerRepository->findSpecialOfferForToday()->getDiscount()) ?? 1;
+        return $product->getPrice() * min($currentUserDiscount, $todaysDiscount);
+    }
 }
